a6d7e3b8e436720b142ad0b5de3a80a715cab43e
Jeff Ripke <jripke74@gmail.com>
2024-07-27 08:24:13 -0700

    add app.use()

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,14 @@ require('dotenv').config();
 
 const express = require('express');
 
+// express app
 const app = express();
+
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
 app.get('/', (req, res) => {
   res.json({ msg: 'Welcome to the app' });
