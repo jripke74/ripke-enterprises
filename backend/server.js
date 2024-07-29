1b960055472951a38a22e1f8cd14df23bb092e9b
Jeff Ripke <jripke74@gmail.com>
2024-07-29 08:24:07 -0700

    add db connection

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,31 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
+const workoutRoutes = require('./routes/workouts');
 
 // express app
 const app = express();
 
 // middleware
+app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
-app.get('/', (req, res) => {
-  res.json({ msg: 'Welcome to the app' });
-});
+// routes
+app.use('/api/workouts', workoutRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log('Listening on port 4000');
-});
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('Connected to DB and listening on port 4000');
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
