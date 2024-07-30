69f94899e2fdca1a29afd54cb36b2ffe13a32d42
Jeff Ripke <jripke74@gmail.com>
2024-07-30 12:15:08 -0700

    add Navbar.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // pages & components
-import Home from './pages/Home';
+import Home from './pages/Home.jsx';
+import Navbar from './components/Navbar.jsx';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
