f8b7d694a86d0791e698d5aac55300e7dee236ff
Jeff Ripke <jripke74@gmail.com>
2024-08-05 08:06:53 -0700

    add Signup

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,13 +1,15 @@
-const { useState } = require('react');
+import { useState } from 'react';
+import { useSignup } from '../hooks/useSignup';
 
 const Signup = () => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const { signup, error, isLoading } = useSignup();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log(email, password);
+    await signup(email, password);
   };
 
   return (
@@ -26,6 +28,7 @@ const Signup = () => {
         value={password}
       />
       <button>Sign up</button>
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
